0fb1209680b57040dee5f0247d5e22a07ec5d4c5
Eflsane <kireevroma01@yandex.ru>
2021-06-19 21:31:24 +0400

    register

--- app/src/main/java/com/example/iherb/Register.java
+++ app/src/main/java/com/example/iherb/Register.java
@@ -3,12 +3,23 @@ package com.example.iherb;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
 
 import com.example.iherb.db.DatabaseHelper;
 import com.example.iherb.db.HelperFactory;
+import com.example.iherb.db.dao.UserDao;
 import com.example.iherb.db.entities.User;
 
 public class Register extends AppCompatActivity {
+    EditText firstName;
+    EditText lastName;
+    CalendarView birthDateCalendar;
+    EditText login;
+    EditText password;
+    EditText sex;
+
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +27,18 @@ public class Register extends AppCompatActivity {
             setContentView(R.layout.activity_register);
             HelperFactory.setHelper(this);
             DatabaseHelper databaseHelper = HelperFactory.getHelper();
+
+            firstName = findViewById(R.id.editTextFirstName);
+            lastName = findViewById(R.id.editTextLastName);
+            birthDateCalendar = findViewById(R.id.calendarViewDateOFBirth);
+            login = findViewById(R.id.editTextLoginRegister);
+            password = findViewById(R.id.editTextPasswordRegister);
+            sex = findViewById(R.id.editTextSex);
         }
 
         public void registerButtonClick(View view) {
 
+
+            HelperFactory.getHelper().getUserDao().create(new User(firstName.getText().toString(), lastName.getText().toString(), ));
         }
 }
