f043429aaaf4d257135c0a51457f40f7881a8b43
WirWollen <riddeneez@gmail.com>
2021-06-19 18:19:28 +0400

    Drawer

---
+++ app/src/main/java/com/example/iherb/ItemModel.java
@@ -0,0 +1,13 @@
+package com.example.iherb;
+
+public class ItemModel {
+
+    public int icon;
+    public String name;
+
+    // модель данных используемая в адаптере DrawerItemCustomAdapter
+    public ItemModel(int icon, String name) {
+        this.icon = icon;
+        this.name = name;
+    }
+}
